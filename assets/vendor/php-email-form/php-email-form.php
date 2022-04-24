488c39b42918d2c52d4d55a8f0752fa81488fc49
Jose <enjoyjos@hotmai.com>
2022-04-24 20:08:50 +0200

    Subida de archivos

---
+++ assets/vendor/php-email-form/php-email-form.php
@@ -0,0 +1,24 @@
+﻿<?PHP
+$to = "info@joyjos.eu";
+$subject = "JOYJOS";
+$from = $_POST['email'];
+$message = "Nombre: " . $_POST['name'];
+$message .= "\nEmail: " . $from;
+$message .= "\n\nMensaje: " . $_POST['message'];
+$headers = "From: $e";
+$headers .= "\nReply-To: $e";
+
+if (filter_var($from, FILTER_VALIDATE_EMAIL)) {
+    $headers = ['From' => ($name?"<$name> ":'').$from,
+            'X-Mailer' => 'PHP/' . phpversion()
+           ];
+
+    mail($to, $subject, $message."\r\n\r\nfrom: ".$_SERVER['REMOTE_ADDR'], $headers);
+    die('OK');
+    
+} else {
+    die('Email inválido');
+}
+
+?>
+success
---
+++ forms/contact.php
@@ -0,0 +1,26 @@
+<?php
+
+  // Receiving email address
+  $receiving_email_address = 'info@joyjos.eu';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'No se puede cargar la librería "PHP Email Form"!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+
+?>
